e01786de01ffb87dbf31c641059a7a7f2ee0d5db
detroit.vv@gmail.com <3e4r5t6y>
2019-01-01 19:36:11 +0200

    executable

---
+++ src/main/java/lviv/lvivboot/LvivBootApplication.java
@@ -0,0 +1,14 @@
+package lviv.lvivboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvivBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LvivBootApplication.class, args);
+    }
+
+}
+
