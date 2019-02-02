ddc63ddce558e9315dd2e9b5910e8d09719809a4
Vasiliy Kylik <detroit.vv@gmail.com>
2019-02-02 11:26:16 +0200

    DeveloperService

--- src/main/java/lviv/lvivboot/LvivBootApplication.java
+++ src/main/java/lviv/lvivboot/LvivBootApplication.java
@@ -2,8 +2,10 @@ package lviv.lvivboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LvivBootApplication {
 
     public static void main(String[] args) {
