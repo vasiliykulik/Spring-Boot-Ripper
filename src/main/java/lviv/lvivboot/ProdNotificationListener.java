91596d668d6e5d4ec4533657e6dc64e33fa95173
Vasiliy Kylik <detroit.vv@gmail.com>
2019-02-25 15:15:28 +0200

    ProdNotificationListener

---
+++ src/main/java/lviv/lvivboot/ProdNotificationListener.java
@@ -0,0 +1,10 @@
+package lviv.lvivboot;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 25.02.2019.
+ */
+// @Component
+public class ProdNotificationListener {
+}
